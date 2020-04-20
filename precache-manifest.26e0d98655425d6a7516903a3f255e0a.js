842598a54323b3038fcefd69cc392b92b7db8ab8
Nirzhuk <john.voiden@gmail.com>
2020-04-20 19:38:29 +0200

    Updates

---
+++ precache-manifest.26e0d98655425d6a7516903a3f255e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01c8bfd3eeb81fe478fa2811df5e578",
+    "url": "/openforge_visualizer/index.html"
+  },
+  {
+    "revision": "6333888ed4a42fa76d83",
+    "url": "/openforge_visualizer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b247fc7f4f6355e580a9",
+    "url": "/openforge_visualizer/static/js/2.09ca05c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/openforge_visualizer/static/js/2.09ca05c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6333888ed4a42fa76d83",
+    "url": "/openforge_visualizer/static/js/main.930b2a9f.chunk.js"
+  },
+  {
+    "revision": "45d94e08a93dbce8601d",
+    "url": "/openforge_visualizer/static/js/runtime-main.b5297c66.js"
+  }
+]);
